2bca44ec89fc692d34dab69cddae09b055e60412
IlonaWishec <ilonawishec.management@gmail.com>
2024-12-01 20:46:21 +0300

    module_7_3.py

--- module_7_3.py
+++ module_7_3.py
@@ -29,10 +29,8 @@ class WordsFinder:
             result[file_name] = words.count(word.lower())
         return result
 
-finder1 = WordsFinder('Rudyard Kipling - If.txt',)
-
-finder1 = WordsFinder('Mother Goose - Monday’s Child.txt',)
-print(finder1.get_all_words())
-print(finder1.find('Child'))
-print(finder1.count('Child'))
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words()) # Все слова
+print(finder2.find('TEXT')) # 3 слово по счёту
+print(finder2.count('teXT')) # 4 слова teXT в тексте всего
 
